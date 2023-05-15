a36728a2994724d169b15800015e5ce0991a2768
sabrinarod26 <srodrig1@umbc.edu>
2023-05-15 15:10:12 -0400

    commit

--- cart.php
+++ cart.php
@@ -304,8 +304,9 @@ if ($result->num_rows > 0) {
         echo "<td>" . $row['title'] . "</td>";
         echo "<td>" . $row['price'] . "</td>";
         echo "<td>" . $row['quantity'] . "</td>";
-        echo "<td><form method='POST' action='cart.php'><input type='hidden' name='cart_id' value='" . $row["id"] . "'>
-        <input type='submit' name='delete_item' value='Delete'></form></td>";
+        echo "<td><form method='POST' action='cart.php'>
+        <input type='hidden' name='cart_id' value='" . $row["id"] . "'>
+        <input type='submit' name='delete_item' value='Delete' action='cart.php'></form></td>";
         echo "</tr>";
     }
     echo "</table>";
